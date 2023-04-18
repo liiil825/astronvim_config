15da93ef718ed27eb8ec70c6b196cc0d5c67de79
lizhi <liiil825.2012@gmail.com>
2023-04-19 06:07:34 +0800

    add emacs key mappings

--- mappings.lua
+++ mappings.lua
@@ -1,5 +1,9 @@
 return {
+  n = {
+    ['<C-a>'] = { '$', desc = 'Move the cursor to the end of the line' },
+    ['<C-i>'] = { '^', desc = 'Move the cursor to the beginning of the line' },
+  },
   v = {
-    ["<leader>y"] = { '"+y', desc = "Copy to clipboard" },
+    ['<leader>y'] = { '"+y', desc = 'Copy to clipboard' },
   },
 }
--- options.lua
+++ options.lua
@@ -1,5 +1,7 @@
 return {
   opt = {
+    wrap = true,
+    textwidth = 80,
     clipboard = '',
     statuscolumn = '%l %r', -- Show actual and relative line numbers
   },
