4720d0359ebe8411e6ee93453a7b71dc2a1708f2
lizhi <liiil825.2018@gmail.com>
2023-04-19 16:31:43 +0800

    change the split key mapping

--- mappings.lua
+++ mappings.lua
@@ -3,6 +3,9 @@ return {
     ['<C-a>'] = { '$', desc = 'Move the cursor to the end of the line' },
     ['<C-i>'] = { '^', desc = 'Move the cursor to the beginning of the line' },
     ['<leader>H'] = { '<cmd>checkhealth<cr>', desc = 'Run cmd checkhealth' },
+    ['|'] = '',
+    ['\\'] = { '<cmd>vsplit<cr>', desc = 'Vertical split' },
+    ['-'] = { '<cmd>split<cr>', desc = 'Horizontal split' },
   },
   v = {
     ['<leader>y'] = { '"+y', desc = 'Copy to clipboard' },
