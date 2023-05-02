21c7eb2e422abb9bec9564ca45e4485ef2599bec
lizhi <liiil825.2012@gmail.com>
2023-05-02 10:35:55 +0800

    chore: statuscolumn not work

--- options.lua
+++ options.lua
@@ -3,7 +3,7 @@ return {
     wrap = true,
     textwidth = 80,
     clipboard = "",
-    statuscolumn = "%l %r", -- TODO notwork -- Show actual and relative line numbers
+    -- statuscolumn = "%l %r", -- TODO notwork -- Show actual and relative line numbers
   },
   g = {
     maplocalleader = ",",
