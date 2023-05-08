bf589e5397044dca5752ef26708059c8e92c3932
lizhi <liiil825.2012@gmail.com>
2023-05-08 20:44:16 +0800

    fix(lsp): vue langager server

--- init.lua
+++ init.lua
@@ -1,21 +1,16 @@
 require 'user.machine'
 
 return {
-  -- lsp = {
-  --   config = {
-  --     volar = function(opts)
-  --       opts.filetypes = {
-  --         'typescript',
-  --         'typescriptreact',
-  --         'typescript.tsx',
-  --         'javascript',
-  --         'javascriptreact',
-  --         'javascript.jsx',
-  --         'vue',
-  --       }
-  --       return opts
-  --     end,
-  --   },
-  -- },
+  lsp = {
+    config = {
+      volar = function(opts)
+        opts.filetypes = {
+          'typescript',
+          'vue',
+        }
+        return opts
+      end,
+    },
+  },
   colorscheme = 'tokyonight',
 }
